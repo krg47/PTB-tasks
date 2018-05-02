5c7eebc64bdffedb5db1f50f5bafa8522b38caeb
GPNUSER <krg47@pitt.edu>
2018-05-02 16:00:11 -0400

    Fancy README

--- Task2/README.m
+++ Task2/README.m
@@ -1,28 +1,45 @@
 % Task 2
 
-% Design
+% Design:
 
-    % To launch, click "Run" in task2.m or call function task2 as in example below.                                                                                                                                                       the "=" key to start the experiment
-    % A block is a group of repetitions. 
-    % There will be a short break between blocks.
-    % The valence (% emotion) of the face only changes between blocks.
-    % The number of repetitions is the number of sequences the participant
-        % will see per block.
-    % The time between each face display is the Interstimulus Interval
-        % (ISI).
-    % The time between sequences is the Intertrial Interval (ITI).
-    % The time between blocks is the Interblock Interval (IBI).
-    % The participant will press keys (1-4) corresponding to the location of a face 
-        % in a randomly generated sequence.
-    % The first two blocks are used to establish a baseline speed, using
-        % neutral faces.
-    % The speed of the following blocks is compared to the average of the baseline.
+    % To launch the task, click "Run" in task2.m or call the function "task2" as in example below. 
+    % Press the "=" key to start the experiment, as prompted.
+    % A face will appear in one of the fours squares on the screen.
+    % Select the corresponding number (1,2,3,or 4) in relation to what square the face appears.
+        % 1 is the left most square.
+        % 2 is the left center square.
+        % 3 is the right center square.
+        % 4 is the right most square.
+    % Once the key is pressed, the face will disappear, after a short Interstimulus Interval, 
+    % the same face will appear elsewhere.
+        % Interstimulus Interval (ISI) is a moderate (0.1 - 0.4 seconds) pause between two displayed faces.
+    % After a certain number of faces have been shown, there will be a pause (Intertrial Interval). 
+    % The number of faces shown between Intertrial Intervals is considered a sequence. 
+        % Intertrial Interval (ITI) is a brief (0 - 0.1 seconds) pause between sequences.
+        % A group of sequences is considered a block.
+    % After a certain number of sequences have been presented, the Interblock Interval will pass.
+        % Interblock Interval (IBI) is a lengthy (4 - 6 seconds) pause between two groups of sequences.  
+    % At least two blocks must be presented before the faces change emotion and the valence only changes between blocks.
+        % The valence is the percentage of emotion on each face.
+        % The first two blocks are used to establish a baseline speed, using neutral faces.
+    % The speed that it takes each block (the average of sequence speeds) will be
+    % recorded and used to determine the valence of the next block.
     % A 5% change in speed will result in a 10% change in face emotion.
-    % Based on the difference in speeds, the valence of the image will become 
-        % happier or sadder by a percentage relative to the neutral valence in the next block.
     % Initially, faster speeds result in increasingly negative valences (reverse learning).
     % At some point in the experiment, learning is reversed so that faster
-        % speeds result in increasingly positive valences (forward learning).
+    % speeds result in increasingly positive valences (forward learning).
+    % All of the sequences are generated randomly, but must abide by these parameters:
+        % The sequences cannot have consecutive, repeating numbers.
+        % The sequences cannot have the same number at the beginning and end (due to cycling).
+        % The sequences must contain every number - each must contain 1,2,3,and 4.
+    % At random, the sequence will cycle. The last number in the sequence
+        % will be move to the first position. (i.e.(1,2,3,4,1,2,3) -> (3,1,2,3,4,1,2))
+    % To model Markov Chains, a violation rate (a percentage) can be added. Another
+    % sequence will be inserted at random. In other words, subjects must
+    % learn the sequence so well that they can identify when the
+    % sequence is altered. 
+    % The test subject must obtain a certain accuracy to be considered for
+    % analysis. 
     % The program records the following data from the task in a file titled "<subect_ID>.mat":
         % Image displayed
         % Response time
@@ -48,19 +65,18 @@
 % File/Directory Descriptions
 
     % Working directory should include these files/folders:
-    % task2.m - main body of the program, calls all other functions, responsible for data storage
-    % sequencer.m - randomly generates sequences following these parameters:
-        % There are no repeating numbers
-        % The first and last numbers of the sequence are different (so
-            % there are no repeats when "cycling")
-        % All numbers 1-4 are used
-    % waittostart.m - sets up the screen for display, and waits until "=" is pressed to begin the task 
-    % squares.m - displays faces and squares in which they appear
-    % analyze.m - calculates the percent change in valence based on the response time
-    % Faces - folder containing the pool of faces from which the program selects
-    % The Faces/ folder should store faces at :
-        % "Faces/<sex>/<race>/<model_no>/<emotion_spectrum>/" with face files as
-        % .jpg images sorted from happiest faces to saddest faces.
+        % task2.m - This is the main body of the program, and it calls all
+            % of the other functions. It is responsible for data storage as well.
+        % waittostart.m - This sets up the screen for display, and waits until "=" is pressed to begin the task 
+        % sequencer.m - This randomly generates sequences following the aformentioned parameters.
+        % squares.m - This displays the squares where the faces are positioned as well as the faces. 
+        % analyze.m - This calculates the percent change in valence based
+            % on the response time and impliments the appropriate change in emotion.
+        % Faces - this is a folder of morphed and cropped faces from which the program selects.
+            % The Faces folder should store faces at :
+                % "Faces/<sex>/<race>/<model_no>/<emotion_spectrum>/" with face files as
+                % .jpg images sorted sequentially from one emotion to the other. (i.e. the Happy to Sad folder should
+                % have their faces stored from most happy to most sad)
 
 
 % Example use
