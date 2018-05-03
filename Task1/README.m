09f05ec0c4828436a35098dc547f1b32b69b58b5
GPNUSER <krg47@pitt.edu>
2018-05-03 14:35:30 -0400

    Even Fancier README

--- Task1/README.m
+++ Task1/README.m
@@ -5,35 +5,40 @@
     % To launch the task, click "Run" in task1.m or call the function "task1" as in the example below. 
     % Press the "=" key to start the experiment, as prompted.
     % A face will appear, and two randomly selected names will appear underneath the face.
-        % Names are pull from a list.
+        % Names are pulled from a list.
     % The participant will select which name they think matches the face by 
     % clicking the left or right arrow key.
         % The left arrow key corresponds to the left name and the right key
         % arrow key corresponds to the right name.
-        % There is no correct answer; the names that appear underneath each face are random.
-    % When a key is selected, the face and names will disappear, the Interstimulus Interval will pass, and another
+        % NOTE: There is no correct answer; the names that appear underneath each face are random.
+    % When a key is selected, the face and names will disappear. After a short Interstimulus Interval,  another
     % face and two more names will appear.
-    % The Interstimulus Interval (ISI) is the time between the disappearance
-    % of one face and the appearance of another.
+        % The Interstimulus Interval (ISI) is the time between the disappearance
+        % of one face and the appearance of another.
     % Depending on what key was selected, the next face that appears should
-    % have a certain valence.
-        % For example, the left key may produce a neutral face while the
-        % right key produces a sad face.
+    % have a certain valence (e.g., more sad, fearful, happy, etc).
         % The valence is the emotion of the face.
-        % There is a bias that can control which key produces a certain
-        % valence, and it can be modified by changing the Probability of Bias parameter.
-    % The change in valence can be changed in the pop up before the task. 
-    % The three options for valence are:
+    % For example, the left key may produce a neutral face while the
+    % right key produces a sad face. The side that produces this effect is
+    % random and is switched half-way through. 
+        % There is a bias that can control the likelihood that this will
+        % occur. This is encoded by Probability of Bias parameter. For
+        % example, if you set this parameter to 0.8 then 80% of the time
+        % when you click the left arrow it will be sad. 
+    % The pairs of valence can be changed in the pop up before the task. 
+    % The three options for valence pairs are:
         % Neutral to Sad (NS)
         % Neutral to Fearful (NF)
         % Happy to Sad (HS)
     % The program records the following data from the task in a file titled "<test_ID>.mat" that records:
-        % Image numbers
+        % Image numbers (i.e., which image was shown)
         % Group of image (A or B)
-        % Sex of image
-        % Valence of image
-        % Participant's response
-        % Response time
+            % NOTE: Originally, we had two sets of images, but we
+            % eliminated one set of images.
+        % Sex of individual in the image
+        % Valence of individual's face in the image
+        % Participant's response (left or right)
+        % Reaction time
 
 % Adjustable parameters:
 
